142485f884bea7d6515ab46cadbaa01bc43aef82
thibautvangoethem <thibautvangoethem2@gmail.com>
2022-10-16 13:59:48 +0200

    fixed 4 year old off by one error in the code

--- main.cpp
+++ main.cpp
@@ -122,7 +122,7 @@ int main() {
     std::vector<std::vector<int> > pixelArray;
     for (int i = 0; i <size-1 ; ++i) {
         std::vector<int> temp;
-        for (int j = 0; j <size-1 ; ++j) {
+        for (int j = 0; j <size ; ++j) {
             temp.emplace_back(0);
         }
         pixelArray.emplace_back(temp);
